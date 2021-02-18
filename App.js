ea11b364f88e9b1460fdf9121a12ce7492c3ea7e
Tanner Leigh <tannerhleigh@gmail.com>
2021-02-18 12:46:15 -0800

    []

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, 
+  TouchableWithoutFeedback, 
+  TouchableOpacity, 
+  TouchableHighlight,
+  SafeAreaView, Image } from 'react-native';
+
+export default function App() {
+  
+  const handlePress = () => console.log("text clicked");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello World</Text>
+      <StatusBar style="auto" />
+      <TouchableHighlight onPress={() => console.log("Image Touched")}>
+        <View>
+          <Image source={require('./assets/tanner2.png')}/>
+        </View>
+      </TouchableHighlight>
+        {/* <Image source={{ 
+          width: 200, 
+          height: 300,
+          uri: "https://picsum.photos/200/300"}}/> */}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+``
